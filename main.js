9151fa3193ba0cbc1c1b1a3bfe5df8ee4dd98d25
StardustGogeta <liam.gerst@gmail.com>
2017-04-23 23:41:41 -0400

    Replacing IDs with JavaScript
    The last remnants of the old IDs have been swept away. (That's
    impossible! How will the HTML maintain control without the ID
    bureaucracy?) The regional jQuery now has direct control over its
    elements. Fear will keep the checkboxes in line - fear of this library.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 sources = [
-	['ny','New York Times'],
-	['wp','Washington Post'],
-	['wt','Washington Times'],
-	['fn','Fox News'],
-	['cn','CNN News'],
-	['bs','Baltimore Sun'],
-	['ap','Associated Press'],
-	['cs','Chicago Sun-Times'],
-	['bb','BBC News'],
-	['nb','NBC News']
+	['New York Times','white'],
+	['Washington Post','tan'],
+	['Washington Times','cyan'],
+	['Fox News','tomato'],
+	['CNN News','red'],
+	['Baltimore Sun','violet'],
+	['Associated Press','pink'],
+	['Chicago Sun-Times','orange'],
+	['BBC News','lightblue'],
+	['NBC News','lightgreen']
 ];
 
 r = localStorage.getItem("srcs");
 sources.forEach((e,i)=>{
-	input = $('<input>').prop('id', e[0]).prop('type', 'checkbox')
+	input = $('<input>').prop('type', 'checkbox')
 		.change(()=>localStorage.setItem("srcs", getSrcs()))
 		.prop('checked', (r%(2**(i+1)))>(2**i)-1);
-	lbl = $('<label>').prop('id', e[0]+'2').text(e[1]).prepend(input).appendTo('#pick');
+	lbl = $('<label>').css('color', e[1]).text(e[0]).prepend(input).appendTo('#pick');
 });
 
 $("#in").keyup(e => {
@@ -27,7 +27,7 @@ $("#in").keyup(e => {
 
 function getSrcs() {
 	srcs = 0;
-	sources.forEach((e,i)=>{if ($('#'+e[0]).prop('checked')) srcs += 2**i;})
+	$('#pick input').each((i,e) => {if (e.checked) srcs += 2**i;})
 	return srcs
 }
 
